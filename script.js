7593117d501c89e0f6bd91c3329af5244d38aaff
KamranG12 <kamran.jabiyev@aplusa-security.com>
2017-04-27 17:18:29 +0400

    obyekt_accordion

---
+++ script.js
@@ -0,0 +1,26 @@
+var x=0;
+$.fn.Accordion=function(){
+	var self=this;
+	this.click(function() {
+        $('.panel-body').each(function() {
+            $(this).slideUp(500);
+        });
+        x=$(this).find('.panel-body').css('display');
+        if(x==='block'){
+       $(this).find('.panel-body').slideUp(500);
+      }else{
+        $(this).find('.panel-body').slideToggle(500);
+      }
+    });
+
+}
+
+
+
+
+
+
+
+$(document).ready(function() {
+      $('.panel').Accordion();
+})
